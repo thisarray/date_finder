be638922de9ff3b1d36c5081eb69d5ee5b442176
thisarray <59105532+thisarray@users.noreply.github.com>
2025-03-24 17:55:55 -0700

    Return a list of matching dates.

--- reuse.py
+++ reuse.py
@@ -3,27 +3,30 @@
 import datetime
 import unittest
 
-YEAR_LIMIT = 2000
+EPOCH = 2000
+"""Integer year of when to stop the search."""
 
 def find_year(target):
-    """Return a datetime.date object with the same day of the week as target.
+    """Return a list of dates with the same day of the week as target.
 
     Args:
         target: datetime.date object to match.
     Returns:
-        datetime.date object with the same day of the week as target.
+        List of datetime.date objects in years less than target and greater
+        and equal to EPOCH with the same day of the week as target.
     """
     if not isinstance(target, datetime.date):
         raise TypeError('target must be a datetime.date.')
 
+    result = []
     year = target.year - 1
-    while year > YEAR_LIMIT:
+    while year >= EPOCH:
         date = datetime.date(year, target.month, target.day)
         if date.weekday() == target.weekday():
-            return date
+            result.append(date)
         year -= 1
 
-    return None
+    return result
 
 class _UnitTest(unittest.TestCase):
     def test_find_year(self):
@@ -31,7 +34,13 @@ class _UnitTest(unittest.TestCase):
         for value in [None, 42, '', []]:
             self.assertRaises(TypeError, find_year, value)
         self.assertEqual(find_year(datetime.date(2025, 1, 1)),
-                         datetime.date(2020, 1, 1))
+                         [datetime.date(2020, 1, 1),
+                          datetime.date(2014, 1, 1),
+                          datetime.date(2003, 1, 1)])
+        self.assertEqual(find_year(datetime.date(2025, 3, 1)),
+                         [datetime.date(2014, 3, 1),
+                          datetime.date(2008, 3, 1),
+                          datetime.date(2003, 3, 1)])
 
 if __name__ == '__main__':
     import argparse
@@ -41,12 +50,14 @@ if __name__ == '__main__':
         help='integer target year (defaults to %(default)s)')
     args = parser.parse_args()
 
-    if args.year > YEAR_LIMIT:
+    if args.year >= EPOCH:
         for target in [datetime.date(args.year, 1, 1),
                        datetime.date(args.year, 3, 1)]:
-            match = find_year(target)
-            if isinstance(match, datetime.date):
-                print('{} matched {}'.format(target.isoformat(), match.isoformat()))
+            matches = find_year(target)
+            if len(matches) > 0:
+                print('{} matched'.format(target.isoformat()))
+                for match in matches:
+                    print('\t{}'.format(match.isoformat()))
     else:
         suite = unittest.defaultTestLoader.loadTestsFromTestCase(_UnitTest)
         unittest.TextTestRunner(verbosity=2).run(suite)
